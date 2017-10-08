df4f09ab60f2080312fd9e015f0d3227d4cc9830
Ken <ken@KendeMacBook-Pro.local>
2017-10-08 15:17:03 +0800

    update config

--- server/util/config_production.js
+++ server/util/config_production.js
@@ -2,10 +2,18 @@
  * Created by Ken on 15/5/23.
  */
 exports.db = {
-  user: 'user',
-  password: 'pwd',
-  database: 'db',
+  user: 'web',
+  password: 'Abc123',
+  database: 'wxgzh',
   host: '127.0.0.1',
   charset: 'utf8mb4', // support emoji from wechat
 };
 
+exports.wechat = {
+  token: 'mytoken',
+  appid: 'wx450f67f9601fba01',
+  appsecret: '153a47c5946325ea72632e19f4348917',
+  encodingAESKey: '',
+  checkSignature: true,
+  templateId: '',
+};
